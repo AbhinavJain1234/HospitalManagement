b1ed3f6e429c0a16b9ed54b8ec166e104bef50e9
AbhinavJain1234 <abhinavj3001@gmail.com>
2025-10-04 16:23:01 +0530

    Project Initialized

---
+++ src/test/java/com/abhinav/hospitalManagement/HospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.hospitalManagement.HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
